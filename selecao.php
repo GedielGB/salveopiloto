3393795b3b6c3916c9dc455d6b5438440d0374c4
GedielGB <83519908+GedielGB@users.noreply.github.com>
2022-04-07 16:48:12 -0300

    Update selecao.php

--- selecao.php
+++ selecao.php
@@ -3,20 +3,17 @@ session_start();
 ?>
 
 <!DOCTYPE html>
-<html>
-    
+<html>    
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Salve o piloto - seleção</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
 <body>
-
     <div class='container-fluid'>
         <div class='texto';>
              <h1>SELECIONE SUA AERONAVE</h1>
